6573954715de1e77789d9f6f0af9cfc467bf4ed6
luiz felippe klein <luizx302@gmail.com>
2025-02-12 15:54:44 -0300

    desafio concluido

--- contaBanco/src/contaTerminal.java
+++ contaBanco/src/contaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero = sc.nextInt();
+        sc.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = sc.nextLine();
+
+        System.out.println("O nome do cliente: ");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Saldo da conta: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        sc.close();
+
     }
 }
